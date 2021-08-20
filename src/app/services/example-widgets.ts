cafc39dc987552e853a6dc0a59c600f218928955
Sebastián Chicoma Sandmann <schicoma@bytesw.com>
2021-08-20 00:40:20 -0500

    remove files

--- src/app/services/example-widgets.ts
+++
@@ -1,8 +0,0 @@
-import { Widget } from 'src/app/models/widget';
-
-export const myWidgets: Widget[] = [
-    { "id": 1629435659150, "type": "greetings" },
-    { "id": 1629435661805, "type": "image", "url": "https://steamuserimages-a.akamaihd.net/ugc/1814365766212573958/04B7867DCE7F7DC885EF9D2B31FCDC9EF2D03443/?imw=512&&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false" },
-    { "id": 1629435669894, "type": "text", "text": "<h1><strong>The Samurai Doge</strong></h1>" },
-    { "id": 1629435691843, "type": "text", "text": "<p>The samurai <span style=\"background-color: rgb(255, 255, 204);\">doge</span> wallpaper or the samurai cheems, I cant get out of my mind. Why is he crying? Is his samurai owner leaving him behind? Is he dead? Is there any continuation?<strong> We will never know </strong>🎐</p>" }
-];
